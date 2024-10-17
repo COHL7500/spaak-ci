01770ae8b0b4f4dd501b12ef246f3ce1171858dd
COHL7500 <cohl7500@gmail.com>
2024-10-17 02:43:23 +0200

    feat: implement pagination (for $skip)

--- src/app/api/store-laws/route.ts
+++ src/app/api/store-laws/route.ts
@@ -12,12 +12,24 @@ const prisma = new PrismaClient();
 
 export async function POST() {
     const endpoint = "https://oda.ft.dk/api/Sag?$filter=(typeid eq 3 or typeid eq 5 or typeid eq 9) and periodeid eq 160&$select=id,statusid,titel,titelkort,resume";
+    let allLaws: any[] = [];
+    let skip = 0;
+    const limit = 100;
 
     try {
-        const res = await fetch(endpoint);
-        const data = await res.json();
+        while (true) {
+            const res = await fetch(`${endpoint}&$skip=${skip}`);
+            const data = await res.json();
 
-       const createLaws = data.value.map((law: any) => {
+            if (data.value.length === 0) {
+                break;
+            }
+
+            allLaws = allLaws.concat(data.value);
+            skip += limit;
+        }
+
+       const createLaws = allLaws.map((law: any) => {
             return prisma.law.create({
                 data: {
                     id: law.id,
@@ -31,7 +43,7 @@ export async function POST() {
 
        await Promise.all(createLaws);
 
-       return NextResponse.json({ message: "Laws stored", data });
+       return NextResponse.json({ message: "Laws stored", data: allLaws });
     } catch (error) {
         return NextResponse.json({ status: 500, body: 'Internal Server Error' });
     } finally {
